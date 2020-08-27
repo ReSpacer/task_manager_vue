a74a55fa1b9be32b63f1cc5b09f001c5252bf0ba
Leonid Arhipov <trupikk764@gmail.com>
2020-08-27 21:50:10 +0700

    Created a project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
+import store from './store/store'
+import router from './router/router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
